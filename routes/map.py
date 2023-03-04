95c2c1278d794309b711b03c8711bfb669e486db
Lyem <lyemningthou5@gmail.com>
2023-03-04 10:36:44 -0800

    Generate map.html

--- routes/map.py
+++ routes/map.py
@@ -27,5 +27,6 @@ def mapRoutes(app):
         m = folium.Map(list(loc), zoom_start=17)
         geometries = osmnx.geometries.geometries_from_point(loc, tags={"building": True}, dist=1000)
         folium.GeoJson(data=geometries['geometry']).add_to(m)
+        m.save("map.html")
         map_file = m._repr_html_()
         return map_file
