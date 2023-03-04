118b451e5afd8a4e9c3f97897bbd48b06643d0cc
Lyem <lyemningthou5@gmail.com>
2023-03-03 22:21:07 -0800

    a

--- routes/map.py
+++ routes/map.py
@@ -12,12 +12,11 @@ def mapRoutes(app):
     @app.route('/latlontomap')
     def LatLontoMap():
         args = request.args
-        lat = args.get("lat")
+        lat = float(args.get("lat"))
         lon = float(args.get("lon"))
         loc = (lat, lon)
         m = folium.Map(list(loc), zoom_start=17)
         geometries = osmnx.geometries.geometries_from_point(loc, tags={"building": True}, dist=1000)
-        osmnx.geometries.geometries_from_place(lat, tags={"building": True}, dist=1000)
         folium.GeoJson(data=geometries['geometry']).add_to(m)
         map_file = m._repr_html_()
         return map_file
