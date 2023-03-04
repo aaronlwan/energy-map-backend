24319183d507ce1ed0e760d6f0efa69ebc030e03
Lyem <lyemningthou5@gmail.com>
2023-03-03 21:46:52 -0800

    Fix LatLontoMap

--- routes/map.py
+++ routes/map.py
@@ -9,13 +9,14 @@ def mapRoutes(app):
     @app.route('/citytolatlon')
     def cityToLatLon():
         return "hello"
+    @app.route('/latlontomap')
     def LatLontoMap():
         args = request.args
-        lat = args.get("lat")
-        lon = args.get("lon")
+        lat = float(args.get("lat"))
+        lon = float(args.get("lon"))
         loc = (lat, lon)
         m = folium.Map(list(loc), zoom_start=17)
         geometries = osmnx.geometries.geometries_from_point(loc, tags={"building": True}, dist=1000)
         folium.GeoJson(data=geometries['geometry']).add_to(m)
-        map_file = m.save("map.html")
+        map_file = m._repr_html_()
         return map_file
