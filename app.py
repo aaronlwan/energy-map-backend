1fda94ece14d427a3983563079edb268e38ce884
Lyem <lyemningthou5@gmail.com>
2023-03-04 11:08:43 -0800

    Add CORS

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from routes.map import mapRoutes
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def root():
     return "test"
 
