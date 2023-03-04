2be54fe4aac1fe6a2e994986a614f80047d4b359
Lyem <lyemningthou5@gmail.com>
2023-03-03 21:25:25 -0800

    Create LatLontoMap function

--- routes/map.py
+++ routes/map.py
@@ -1,4 +1,22 @@
+import folium
+import osmnx
+import geojson
+import geopandas
+from flask import request
+import jsonify
+
 def mapRoutes(app):
     @app.route('/citytolatlon')
     def cityToLatLon():
         return "hello"
+    def LatLontoMap():
+        args = request.args
+        lat = args.get("lat")
+        lon = args.get("lon")
+        loc = (lat, lon)
+        m = folium.Map(list(loc), zoom_start=17)
+        geometries = osmnx.geometries.geometries_from_point(loc, tags={"building": True}, dist=1000)
+        folium.GeoJson(data=geometries['geometry']).add_to(m)
+        m.save("map.html")
+
+        return jsonify({'status': 'success', 'message': 'Map generated successfully!'})
