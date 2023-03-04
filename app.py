8492c34c3d1e6a069abe2f6d4796dfa52bd835cd
aaronlwan <aaronwan@stanford.edu>
2023-03-03 21:19:03 -0800

    initialize map routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from routes.map import mapRoutes
 
 
 app = Flask(__name__)
@@ -6,6 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return "test"
+
+mapRoutes(app)
+
 
 if __name__ == '__main__':
     app.run()
